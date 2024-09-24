c32cfa68b0e7b3a1c6454f0f79eaefd5f0a7580a
Dmytro Riabtsun <riabtsun.dv@gmail.com>
2024-09-24 17:41:37 +0300

    Added new pages

--- home-work-7/js/script.js
+++ home-work-7/js/script.js
@@ -3,33 +3,18 @@ let minRangePrice = document.querySelector('#minRangePrice')
 let maxPriceInput = document.querySelector('#maxPrice')
 let maxRangePrice = document.querySelector('#maxRangePrice')
 
-
 minPriceInput.value = minRangePrice.value
 maxPriceInput.value = maxRangePrice.value
-minRangePrice.addEventListener('change', () => {
-  setPriceValues(minPriceInput, minRangePrice)
-})
-maxRangePrice.addEventListener('change', () => {
-  setPriceValues(maxPriceInput, maxRangePrice)
-})
 
-function setPriceValues(priceInput, priceRange) {
-  priceInput.value = priceRange.value
-  priceInput.input = priceRange.value
-  priceRange.addEventListener('change', () => {
-    priceInput.value = priceRange.value
-    checkCorrectPrice(priceInput.value, priceRange.value)
+function syncInputs(rangeInput, numberInput) {
+  rangeInput.addEventListener('input', function() {
+    numberInput.value = this.value
   })
-  priceInput.addEventListener('input',()=>{
-    priceRange.value = priceInput.value
-  })
-  priceInput.addEventListener('change',()=>{
-    priceRange.value = priceInput.value
+  numberInput.addEventListener('input', function() {
+    rangeInput.value = this.value
   })
 }
 
-function checkCorrectPrice(priceInput, priceRange) {
-  Number(priceInput) < Number(priceRange) && alert('Please choose correct price')
-  console.log(priceInput, priceRange, 'hi')
-}
+syncInputs(minPriceInput, minRangePrice)
+syncInputs(maxPriceInput, maxRangePrice)
 
