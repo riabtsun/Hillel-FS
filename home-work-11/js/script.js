2cc23e9858e0a9dfc504ab7d8edae66c74386c7b
Dmytro Riabtsun <riabtsun.dv@gmail.com>
2024-10-10 10:47:10 +0300

    Fixed styles for tablet

--- home-work-11/js/script.js
+++ home-work-11/js/script.js
@@ -3,16 +3,6 @@ let navMenu = document.querySelector('.nav')
 
 menuBtn && menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('active')
-
-  if (menuBtn.classList.contains('active') && document.documentElement.clientWidth < 992) {
-    navMenu.style.display = 'flex'
-    navMenu.style.flexDirection = 'column'
-  } else {
-    navMenu.style.display = 'none'
-  }
-
-  if (document.documentElement.clientWidth >= 992) {
-    navMenu.display = 'flex'
-  }
+  navMenu.classList.toggle('active')
 })
 
