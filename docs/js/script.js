e298f4e974d486f3a05604f198d0a66d68e4c021
Dmytro Riabtsun <riabtsun@i.ua>
2024-10-15 15:57:36 +0300

    refactor doc's page

--- docs/js/script.js
+++
@@ -1,8 +0,0 @@
-let menuBtn = document.querySelector('.menu-btn')
-let navMenu = document.querySelector('.nav')
-
-menuBtn && menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('active')
-  navMenu.classList.toggle('active')
-})
-
