44ae3906a2712ae1f7bf6d5a5a8d55da4d9b67f1
Dmytro Riabtsun <riabtsun.dv@gmail.com>
2024-10-23 17:30:26 +0300

    added header to home-work-15

--- home-work-15/tailwind.config.js
+++ home-work-15/tailwind.config.js
@@ -1,14 +1,46 @@
 /** @type {import('tailwindcss').Config} */
+
+let gap = 20
 module.exports = {
   content: ['*.html'],
-  darkMode:'class',
+  darkMode: 'class',
   theme: {
+    colors: {
+      primary: '#2d3651',
+      accent: '#d66926',
+      highLight: '#d53d27',
+      gray: '#637588',
+      grayLight: '#e5e5e5',
+      grayDark: '#44596e',
+      white: '#fff',
+    },
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      ul: '1340px',
+    },
     extend: {
       textColor: {
-        tomato: 'tomato'
+        tomato: 'tomato',
       },
       fontFamily: {
-        main: ['Roboto', 'sans-serif']
+        sans: ['Roboto', 'sans-serif'],
+        serif: ['Roboto Slab', 'serif'],
+      },
+      fontSize: { base: '16px' },
+      spacing: {
+        gap: gap + 'px',
+        gapSm: `${gap / 2}px`,
+        gapLg: `${gap * 1.5}px`,
+        gapX2: `${gap * 2}px`,
+        gapX3: `${gap * 3}px`,
+        gapX4: `${gap * 4}px`,
+        gapX5: `${gap * 5}px`,
+      },
+      minWidth:{
+        custom: 'min(100% - 40px, 1300px)'
       }
     },
   },
