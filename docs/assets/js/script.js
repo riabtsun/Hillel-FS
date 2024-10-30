d66eb61932d8d3d68e7b837b137f1f24398cfb8d
Dmytro Riabtsun <riabtsun@i.ua>
2024-10-30 22:03:19 +0200

    added home work 18

--- docs/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector('.menu-btn')
-const mobileList = document.querySelector('.header-navList')
-
-window.addEventListener("load", function (){
-    burger.addEventListener('click', function () {
-        this.classList.toggle('active')
-        mobileList.classList.toggle('active')
-    })
-});
