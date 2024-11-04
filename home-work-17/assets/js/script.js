2530cefef5cbb61a3dfffb1e757ba38f5c05b1e8
Dmytro Riabtsun <riabtsun.dv@gmail.com>
2024-11-04 16:09:54 +0200

    finished layout

--- home-work-17/assets/js/script.js
+++ home-work-17/assets/js/script.js
@@ -1,6 +1,9 @@
 let headerMenuBtn = document.querySelector('.menu__btn')
 let headerNav = document.querySelector('.nav')
 let navList = document.querySelector('.nav__list')
+const header = document.querySelector('.header')
+const defaultOffset = 300
+let lastScroll = 0
 
 headerMenuBtn.addEventListener('click', function () {
   this.classList.toggle('active')
@@ -8,4 +11,21 @@ headerMenuBtn.addEventListener('click', function () {
   setTimeout(() => {
     navList.classList.toggle('active')
   }, 200)
+})
+
+const scrollPosition = () =>
+  window.pageYOffset || document.documentElement.scrollTop
+const containHide = () => header.classList.contains('hide')
+
+window.addEventListener('scroll', () => {
+  if (
+    scrollPosition() > lastScroll &&
+    !containHide() &&
+    scrollPosition() > defaultOffset
+  ) {
+    header.classList.add('hide')
+  } else if (scrollPosition() < lastScroll && containHide()) {
+    header.classList.remove('hide')
+  }
+  lastScroll = scrollPosition()
 })
