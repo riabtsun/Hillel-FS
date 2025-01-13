fa8588bc1295996b0bb2554ad7b7c73c2d581a1c
Dmytro Riabtsun <riabtsun@i.ua>
2025-01-13 15:32:01 +0200

    hw 35

--- JS/home-work-34/webpack.config.js
+++ JS/home-work-34/webpack.config.js
@@ -1,10 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
+
+const optimization = () => ({
+  splitChunks: {
+    chunks: 'all',
+  },
+  minimizer: [new CssMinimizerWebpackPlugin(), new TerserPlugin()],
+});
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+console.log('IS DEV:', isDev);
+console.log('IS PROD:', isProd);
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  mode: 'development',
+  mode: isDev ? 'development' : 'production',
   entry: {
     main: './index.js',
     stat: './statistics.js',
@@ -21,16 +39,27 @@ module.exports = {
     },
     extensions: ['.js', '.json', '.jsx', '.ts', '.tsx'],
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
+  optimization: optimization(),
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
     }),
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.png'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new EslintWebpackPlugin({
+      extensions: ['js'], // Визначаємо розширення файлів для перевірки
+      fix: true, // Вмикаємо автоматичне виправлення помилок
+    }),
   ],
   module: {
     rules: [
@@ -60,6 +89,42 @@ module.exports = {
         test: /\.csv$/,
         use: ['csv-loader'],
       },
+      {
+        test: /\.css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '',
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      // {
+      //   test: /\.css$/,
+      //   use: cssLoaders(),
+      // },
     ],
+  },
+  target: 'web',
+  devServer: {
+    port: 4200,
+    hot: false,
   },
 };
